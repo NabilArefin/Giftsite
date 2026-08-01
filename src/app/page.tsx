42d5a4f03efc6873bf76f12adbb18e7ab2694326
Z User <z@container>
2026-08-01 19:51:39 +0000

    f514abaa-41b8-4d6c-9d2f-8ba83ec1bc93

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -565,7 +565,7 @@ export default function GiftPage() {
               you in every little way I can.
             </p>
             <p className="signature reveal reveal--5">
-              Forever your biggest fan&thinsp;&hearts;
+              I&thinsp;&hearts;&thinsp;U
             </p>
           </div>
         </div>
